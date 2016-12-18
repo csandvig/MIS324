7b4a5280842b0893f6460ffa33174f1115150d5d
csandvig <csandvig@wwu.edu>
2016-12-18 13:42:32 -0800

    comicBook2

---
+++ MvcMovie/Controllers/comicBookController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        // GET: ComicBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Today is " + DateTime.Today.DayOfWeek.ToString());
+        }
+
+    }
+}
